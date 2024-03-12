419e7fb3c2c31671bbfed06c8b76417e967310b0
akadot <muriloholiveira@hotmail.com>
2024-03-12 16:21:51 -0300

    :lipstick: Styling sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import Header from "./components/Header";
+import settings from './assets/settings.svg'
+import hash from './assets/hash.svg'
+import trash from './assets/trash.svg'
+import server from './assets/server.svg'
+import search from './assets/search.svg'
+
 import './styles/app.css'
 function App() {
   return (
-    <section className="container">
+    <main className="container">
       <section className="sidebar">
-        <Header/>
+        <header className='headerContainer'>
+          <h1>Memoir</h1>
+          <img src={settings} alt="Settings Icon" />
+        </header>
+
+        <nav className='menu'>
+          <section className='navItem'>
+            <img src={server} alt="Source" />
+            <p>Source</p>
+          </section>
+          <section className='navItem'>
+            <img src={hash} alt="Hash" />
+            <p>Tags</p>
+          </section>
+          <section className='navItem'>
+            <img src={trash} alt="Trash" />
+            <p>Trash</p>
+          </section>
+        </nav>
+
+        <section className='notesContainer'>
+          <section className='notesMenu'>
+            <p>Notes</p>
+            <label htmlFor="search">
+              <input type="text" id='search' placeholder='Search...'/>
+              <img src={search} alt="Search" />
+            </label>
+          </section>
+          <section></section>
+        </section>
       </section>
-    </section>
+    </main>
   );
 }
 
